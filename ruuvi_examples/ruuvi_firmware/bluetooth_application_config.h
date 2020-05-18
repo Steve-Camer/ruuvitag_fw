d76ca3b018e2f44d1fad7d912f4537c996e568a3
Steve-Camer <65403707+Steve-Camer@users.noreply.github.com>
2020-05-18 07:44:49 +0200

    Update bluetooth_application_config.h

--- ruuvi_examples/ruuvi_firmware/bluetooth_application_config.h
+++ ruuvi_examples/ruuvi_firmware/bluetooth_application_config.h
@@ -1,6 +1,6 @@
 #ifndef BLUETOOTH_APP_CONFIG
 #define BLUETOOTH_APP_CONFIG
-#define APPLICATION_DEVICE_NAME         "Vermop"                         /**< BLE name displayed in scan response. */
+#define APPLICATION_DEVICE_NAME         "myOwn"                         /**< BLE name displayed in scan response. */
 #define APPLICATION_DEVICE_NAME_LENGTH  6                               /**< number of characters in above string, excluding null */
 #define APP_DEVICE_NAME                 APPLICATION_DEVICE_NAME         /**< TODO: Refactoring **/
 #define APP_DEVICE_NAME_LENGTH          APPLICATION_DEVICE_NAME_LENGTH
